b92bc05b491f63122e33e6feec4d0509d3e7d4f0
Håkan Eriksson <haken.eriksson@gmail.com>
2014-01-12 21:41:49 +0100

    Bubbles flattening when hitting ground. TODO: Raise the bottom as they flatline.

--- Bubble.cs
+++ Bubble.cs
@@ -25,18 +25,47 @@ namespace Bubbles
         private const double _diameterIncreaseFactor = 1;
 #endif
 
+        private double _radiusX;
+        public double RadiusX
+        {
+            get { return _radiusX; }
+            set
+            {
+                if (value != _radiusX)
+                {
+                    _radiusX = value;
+                    OnPropertyChanged("RadiusX");
+                }
+            }
+        }
+
+        private double _radiusY;
+        public double RadiusY
+        {
+            get { return _radiusY; }
+            set
+            {
+                if (value != _radiusY)
+                {
+                    _radiusY = value;
+                    OnPropertyChanged("RadiusY");
+                }
+            }
+        }
 
         public Bubble(Size GameArea)
         {
             this._gameArea = GameArea;
             Random r = new Random();
-            int diameter = r.Next(40, 200);
-            Size = new Size(diameter, diameter);
+            int radius = r.Next(40, 150);
+            RadiusX = radius;
+            RadiusY = radius;
+
             ColorDiameter = 0;
 
             int margin = 100;
             X = r.Next(margin, (int)_gameArea.Width - margin);
-            Y = 200;
+            Y = r.Next(-radius, 200);
         }
 
         public double ColorDiameter
@@ -46,7 +75,7 @@ namespace Bubbles
             {
                 if ((value != _colorDiameter) && (!IsPopped))
                 {
-                    if (value < Size.Width)
+                    if (value < RadiusY)
                     {
                         _colorDiameter = value;
                     }
@@ -72,7 +101,7 @@ namespace Bubbles
         private double _ax = 0;
         private double _ay = 0;
 
-        private double Mass { get { return (Size.Width * Size.Height); } }
+        private double Mass { get { return (RadiusX * RadiusY); } }
 
         private double _dt = 0.02;
 
@@ -87,27 +116,40 @@ namespace Bubbles
             base.Update();
 
 
-            double gravitationForce = Size.Width * Size.Height * _gravity;
+            double gravitationForce = Mass * _gravity;
 
 
-            double airResistanceForce = -(0.5 * _airResistance * Size.Width * _vy * _vy);
+            double airResistanceForce = -(0.5 * _airResistance * RadiusX * _vy * _vy);
 
 
             double forceY = gravitationForce + airResistanceForce;
 
 
             double dy = _vy * _dt + (0.5 * _ay * _dt * _dt);
-            Y += dy * 100;
+            double yIncrease = dy * 100;
+            if ((Y + yIncrease + RadiusY) < _gameArea.Height)
+            {
+                Y += yIncrease;
+            }
+            else if ((RadiusY + yIncrease / 2) > 2)
+            {
+                Y += yIncrease / 2;
+                RadiusY -= yIncrease / 2;
+                RadiusX += yIncrease;
+            }
+
 
             double newAccelerationY = forceY / Mass;
             double averageAccelerationY = 0.5 * (newAccelerationY + _ay);
             _vy += averageAccelerationY * _dt;
+
+
         }
 
         internal void AddColor(double pressure)
         {
 #if USE_RELATIVE_COLOR_INCREASE
-            ColorDiameter += pressure * Size.Width * _diameterIncreaseFactor;
+            ColorDiameter += pressure * RadiusX * _diameterIncreaseFactor;
 #else
             ColorDiameter += pressure * _diameterIncreaseFactor;
 #endif
